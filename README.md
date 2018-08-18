230a3d6974a9eadd56780459e465effac230b68e
Heasy <heasy@xiaoxindeMacBook-Pro.local>
2018-08-18 19:44:59 +0800

    修复 git

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-starter-kit
+
+    webpack构建 vue 环境
+    
+> ✎ 线上预览
+
+查看：[https://surile.github.io/vue-starter-kit/](https://surile.github.io/vue-starter-kit/ 'vue脚手架')
+
+## 本地运行
+
+        npm install || yarn install
+        npm start || yarn start
+
+
+## 打包上传
+
+    
+        npm build || yarn build
+    
---
+++ src/router.js
@@ -0,0 +1,52 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import Home from './Home'
+import Users from './Home/users'
+import Register from './Account/register'
+import Login from './Account/login'
+import { store } from './store'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        component: Home,
+        beforeEnter: (to, from, next) => {
+            const token = store.state.isLogin
+            if (!token) {
+                next('/login')
+            } else {
+                next()
+            }
+        },
+        children: [
+            {
+                path: '/',
+                component: Users
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: Login,
+        beforeEnter: (to, from, next) => {
+            const token = store.state.isLogin
+            if (token) {
+                next('/')
+            } else {
+                next()
+            }
+        }
+    },
+    {
+        path: '/register',
+        component: Register
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
